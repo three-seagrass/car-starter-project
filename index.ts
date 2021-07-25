59db330d24470d5f5afbbade98fdf2c110917634
three-seagrass <spaaacey.space@gmail.com>
2021-07-26 09:21:16 +1000

    2 files modified

--- index.ts
+++ index.ts
@@ -27,3 +27,37 @@ const car1: Car = new Car(0, 'NQK490');
 const car2: Car = new Car(10, 'QXZ573');
 const car3: Car = new Car(20, 'TRJ650');
 const carList: Car[] = [car1, car2, car3];
+
+const carSelect: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById('car-select')
+);
+const spdOutput: HTMLElement = document.getElementById('car-spd-output');
+const spdChangeInput: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById('new-spd-input')
+);
+const spdChangeBtn: HTMLElement = document.getElementById('new-spd-btn');
+
+for (let i = 0; i < carList.length; i++) {
+  carSelect.innerHTML += `<option value=${i}>${carList[i].rego}</option>`;
+}
+
+carSelect.addEventListener('change', handleCarChange);
+
+function handleCarChange() {
+  const selectedValue = carSelect.value;
+  renderCarSpd(selectedValue);
+}
+
+spdChangeBtn.addEventListener('click', handleChangeSpd);
+
+function handleChangeSpd() {
+  const newSpdValue: number = Number(spdChangeInput.value);
+  let selectedCarValue = carSelect.value;
+
+  carList[selectedCarValue].speed = newSpdValue;
+  renderCarSpd(selectedCarValue);
+}
+
+function renderCarSpd(carIndex) {
+  spdOutput.innerHTML = `Speed: ${carList[carIndex].speed}`;
+}
