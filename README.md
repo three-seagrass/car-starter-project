a12eb0905cebfa3a5f5f56eb2fde26e3b6cc09f3
three-seagrass <spaaacey.space@gmail.com>
2021-07-25 21:40:00 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# car-starter-project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/car-starter-project)
---
+++ index.ts
@@ -0,0 +1,45 @@
+// Import stylesheets
+import './style.css';
+
+// Car Class
+class Car {
+  speed: number;
+  rego: string;
+  maxSpeed: number = 120;
+
+  constructor(_speed: number, _rego: string) {
+    this.speed = _speed;
+    this.rego = _rego;
+  }
+
+  increaseSpeed(spd: number): void {
+    this.speed += spd;
+
+    // This if statement ensures that the speed of the car doesn't go over the maxSpeed that we have set
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+  }
+
+  decreaseSpeed(changeValue: number): void {
+    this.speed = this.speed - changeValue;
+
+    if (this.speed < 0) {
+      this.speed = 0;
+    }
+  }
+
+  setSpeed(value: number) {
+    this.speed = value;
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+  }
+}
+
+// Instantiate the 3 cars and add it to an array
+const car1: Car = new Car(0, 'NQK490');
+const car2: Car = new Car(10, 'QXZ573');
+const car3: Car = new Car(20, 'TRJ650');
+const carList: Car[] = [car1, car2, car3];
